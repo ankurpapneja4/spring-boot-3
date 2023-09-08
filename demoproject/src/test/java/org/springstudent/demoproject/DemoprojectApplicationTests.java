4509b25e4822ffc3bf74cbd6d440eaf83b3c7f0b
Ankur Papneja <ankur.papneja@excelsoftcorp.com>
2023-09-08 09:41:36 +0530

    Setup DemoProject

---
+++ demoproject/src/test/java/org/springstudent/demoproject/DemoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springstudent.demoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
