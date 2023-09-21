53c0adadf1222382153d35607d9df6b12f6a352e
Ankur Papneja <ankur.papneja@excelsoftcorp.com>
2023-09-22 00:10:08 +0530

    SLF4J Configuration

--- demoproject/src/main/java/org/springstudent/demoproject/restcontroller/HealthCheckController.java
+++ demoproject/src/main/java/org/springstudent/demoproject/restcontroller/HealthCheckController.java
@@ -1,5 +1,7 @@
 package org.springstudent.demoproject.restcontroller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
+    private static final Logger log = LoggerFactory.getLogger(HealthCheckController.class);
+
     @GetMapping("/")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("Healthy");
+    }
+
+    @GetMapping("/log")
+    private ResponseEntity<String> checkLogLevel() {
+        log.trace( "TRACE" );
+        log.debug( "DEBUG" );
+        log.info ( "INFO"  );
+        log.warn ( "WARN"  );
+        log.error( "ERROR" );
+
+        return ResponseEntity.ok("Checking Log Level");
     }
 
 }
